ea811910d9fa90f4ec55169bb9e64d2795fce429
CWH <>
2023-05-22 17:13:21 +0800

    更新： 	提交工程

---
+++ MyClock/MyClock/MyClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyClockApp.swift
+//  MyClock
+//
+//  Created by CWH on 2023/5/22.
+//
+
+import SwiftUI
+
+@main
+struct MyClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
